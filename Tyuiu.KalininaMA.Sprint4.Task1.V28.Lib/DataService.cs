ffc9c6b7cc04b2c50b58aac2fa01b9d3f8196bf1
ya_zae <kalininalmaria@gmail.com>
2025-11-03 19:43:00 +0500

    Task1

---
+++ Tyuiu.KalininaMA.Sprint4.Task1.V28.Lib/DataService.cs
@@ -0,0 +1,20 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint4;
+namespace Tyuiu.KalininaMA.Sprint4.Task1.V28.Lib
+{
+    public class DataService : ISprint4Task1V28
+    {
+        public int Calculate(int[] array)
+        {
+            int prArray = 1;
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (array[i] % 2 != 0)
+                {
+                    prArray *= array[i];
+                }
+            }
+            return prArray;
+        }
+    }
+}
---
+++ Tyuiu.KalininaMA.Sprint4.Task1.V28.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.KalininaMA.Sprint4.Task1.V28.Lib;
+namespace Tyuiu.KalininaMA.Sprint4.Task1.V28.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+
+            int[] array = {2, 4, 4, 5, 3, 4, 4, 6, 2, 4, 5, 5, 4, 4  };
+
+            int res = ds.Calculate(array);
+            int wait = 375;
+            Assert.AreEqual(wait, res);
+
+
+        }
+    }
+}
